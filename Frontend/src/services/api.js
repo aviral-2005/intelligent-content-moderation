72dabfe14c63419edc0ed2c07f279398e25910dc
aviral-2005 <tripathiaviral24@gmail.com>
2026-07-31 00:03:57 +0530

    Change API base URL to production endpoint

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://intelligent-content-moderation.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
