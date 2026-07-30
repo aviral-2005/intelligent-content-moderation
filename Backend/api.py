f545b63d53ae06145c936cfd32c27c8f73360535
aviral-2005 <tripathiaviral24@gmail.com>
2026-07-31 00:16:27 +0530

    Add new allowed origin for CORS

--- Backend/api.py
+++ Backend/api.py
@@ -14,6 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://intelligent-content-moderation.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
